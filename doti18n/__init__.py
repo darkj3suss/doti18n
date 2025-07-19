b909c4dab65d45199bf44fa8ebf32eb4179b2eed
darkjesuss <117569331+darkj3suss@users.noreply.github.com>
2025-07-19 21:45:13 +0300

    update package version

--- doti18n/__init__.py
+++ doti18n/__init__.py
@@ -2,7 +2,7 @@ from .locale_data import LocaleData
 from .locale_translator import LocaleTranslator
 from .wrapped import LocaleList, LocaleNamespace, NoneWrapper, PluralWrapper, StringWrapper
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = [
     "LocaleData",
     "LocaleList",
