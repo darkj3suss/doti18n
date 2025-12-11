7a9d2ce52f4d7c6797c6a6bc9534547dd3534007
darkjesuss <117569331+darkj3suss@users.noreply.github.com>
2025-12-11 09:29:57 +0200

    bump to the 0.3.3 version

--- doti18n/__init__.py
+++ doti18n/__init__.py
@@ -1,7 +1,7 @@
 from .locale_data import LocaleData
 from .locale_translator import LocaleTranslator
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __all__ = [
     "LocaleData",
     "LocaleTranslator"
